9962aff6be1aafd0be9bd12a8fdfbfa2dad42b65
Ayush <ayushrambade2006@gmail.com>
2026-03-14 00:09:54 +0530

    Create data_types.php

---
+++ data_types.php
@@ -0,0 +1,17 @@
+<?php
+
+$integer = 10;
+$float = 10.5;
+$string = "PHP Programming";
+$boolean = true;
+$array = array("HTML", "CSS", "PHP");
+
+echo "Integer: $integer <br>";
+echo "Float: $float <br>";
+echo "String: $string <br>";
+echo "Boolean: $boolean <br>";
+
+echo "Array elements: ";
+print_r($array);
+
+?>
